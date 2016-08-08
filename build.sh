b299e42a2383f97ffb83c31b4ecb827dddd2a703
XePeleato <edu@error404software.com>
2016-08-08 21:26:47 +0200

    Added x64 Gcc Toolchain
    I forgot to upload it at the last commit, now build.sh should work as expected

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 LOCAL_DIR=`pwd`
-export PATH=$PATH:prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
-export CROS_COMPILE=aarch64-linux-android-
+export PATH="$PATH:$LOCAL_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin"
+export CROSS_COMPILE="aarch64-linux-android-"
 mkdir -p out
 echo "Paths and Toolchain loaded!"
 make ARCH=arm64 O=out merge_hi6250_defconfig
 make ARCH=arm64 O=out -j8
-printf "\nDone! You'll find the compiled Image at ../out/arch/arm64/boot/Image"
+printf "\nDone! if it compiled correctly, you'll find the compiled Image at ../out/arch/arm64/boot/Image"
 printf "\nThe modules are at ../out/(device)/*.ko"
